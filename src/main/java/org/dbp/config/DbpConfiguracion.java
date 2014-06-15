43a992931f802e5b0d8e67cac7258a66570f7113
david <david@MSI>
2014-06-15 20:49:58 +0200

    configurar la localización, y el sistema de errores del biendeo de jackson.

--- src/main/java/org/dbp/config/DbpConfiguracion.java
+++ src/main/java/org/dbp/config/DbpConfiguracion.java
@@ -1,9 +1,11 @@
 package org.dbp.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -37,5 +39,14 @@ public class DbpConfiguracion extends WebMvcConfigurerAdapter{
      */
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/recursos/**").addResourceLocations("/recursos/");
+		registry.addResourceHandler("/template/**").addResourceLocations("/template/");
 	}
+	
+    @Bean    
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        
+        messageSource.setBasenames("classpath:locale/mensajes","classpath:locale/bindeo");
+        return messageSource;
+    }
 }
